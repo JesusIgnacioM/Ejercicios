354790a7d6e2c7f6b1e3c779809588ddc239b38d
JesusIgnacioM <112433476+JesusIgnacioM@users.noreply.github.com>
2022-09-10 08:50:20 -0600

    Add files via upload

---
+++ EjercicioIIGuiaIVperiodoI.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    float nota;
+    int mayor = 0;
+    int menor = 0;
+    float limite, contador;
+    cout << "Ingrese el limite: ";
+    cin >> limite;
+         while (contador < limite){
+         	contador = contador + 1;
+         	cout << "Contador " << contador << endl;
+		 }
+}

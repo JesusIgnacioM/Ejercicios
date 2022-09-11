e40b2051caba85375e4bcf0f7c4ae1375d1e2be7
JesusIgnacioM <112433476+JesusIgnacioM@users.noreply.github.com>
2022-09-11 10:10:59 -0600

    Add files via upload

---
+++ EjercicioGuiaIIIperiodoII.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std; 
+
+string parImpar(int num): 
+
+
+int main(){ 
+    int numero; 
+    string cadena; 
+    cout << "Ingrese el numero: "; 
+    cin >> numero; 
+    cadena = parImpar(numero); 
+    cout << "El numero es: " << cadena; 
+    return 0; 
+} 
+
+string parImpar(int num){ 
+string resultado; 
+if (num%2==e){ 
+resultado = "par"; 
+} else if (num%2==1){	 
+resultado = "impar";
+} 
+
+return resultado;
+}
+
+

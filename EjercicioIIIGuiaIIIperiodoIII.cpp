ecf40e6d0f4cd6c71fda2be8bfb673aaf05cd1d1
JesusIgnacioM <112433476+JesusIgnacioM@users.noreply.github.com>
2022-09-11 11:13:19 -0600

    Add files via upload

---
+++ EjercicioIIIGuiaIIIperiodoIII.cpp
@@ -0,0 +1,20 @@
+using namespace std; 
+
+int main() 
+
+{
+int opcion = 0; 
+do 
+{
+
+cout << "Ingrese 1 para mostrar mensaje\n"; 
+cout << "Ingrese 2 para hacer algo mäs\n"; 
+cout << "Ingrese ® para salir\n"; 
+
+cout << "Ingrese una opciön a ejecutar\n"; 
+cin >> opcion; 
+}
+while(opcion != 0); 
+system("PAUSE"); 
+return 0;
+}
